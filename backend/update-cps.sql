74620acf59d18d1fec335076e7907059e463f278
ISABELR08 <isarumachorro.742@gmail.com>
2026-06-26 12:41:41 -0600

    casos de rehabilitacion
    se agregaron 3 casos de rehabilitación

---
+++ backend/update-cps.sql
@@ -0,0 +1,2 @@
+UPDATE colonies SET postal_code = '72000' WHERE city ILIKE '%Puebla%' AND name ILIKE '%Centro%';
+UPDATE colonies SET postal_code = '72590' WHERE city ILIKE '%Puebla%' AND name ILIKE '%Manuel%';
