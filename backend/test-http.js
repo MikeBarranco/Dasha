fe3aab2153545cee37cdcfa5fef265d7faaa359c
Isabel <isarumachorro.742@gmail.com>
2026-07-16 23:08:03 -0600

    fix: eliminar scripts temporales

--- backend/test-http.js
+++
@@ -1,1 +0,0 @@
-const http = require('http'); http.get({hostname: 'localhost', port: 3000, path: '/api/v1/me', headers: { Cookie: 'token=FAKE_TOKEN' }}, (res) => { res.on('data', d => process.stdout.write(d)) })
